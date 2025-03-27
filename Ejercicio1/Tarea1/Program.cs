604870e664642105e635832b86bf69c997597819
Marcelo Ontaneda <marcontanedar@gmail.com>
2025-03-27 10:59:37 +0100

    Creación de estructura del programa

--- Ejercicio1/Tarea1/Program.cs
+++ Ejercicio1/Tarea1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace tarea1
+{
+        internal class Program
+    {
+        //public static int pacienteId = 1;
+        public static Random rnd = new Random();
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread paciente = new Thread(()=>Paciente(i));
+                paciente.Start();
+                
+                Thread.Sleep(rnd.Next(1, 10000));
+                
+            }
+            
+            
+
+            
+
+
+            
+        }
+
+        static void Paciente(int id)
+        {
+            Console.WriteLine("Paciente {0} generado",id);
+        }
+    }
+}
