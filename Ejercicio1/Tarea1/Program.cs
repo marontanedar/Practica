8294b5509c7081a099aeb20b697c70f4b0784454
Marcelo Ontaneda <marcontanedar@gmail.com>
2025-03-28 11:45:41 +0100

    control de medicos

--- Ejercicio1/Tarea1/Program.cs
+++ Ejercicio1/Tarea1/Program.cs
@@ -1,34 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace tarea1
 {
-        internal class Program
+    internal class Program
     {
-        //public static int pacienteId = 1;
-        public static Random rnd = new Random();
+        static SemaphoreSlim sem = new SemaphoreSlim(4);
+        static readonly object locker = new object();
+        static Random rnd = new Random();
+        static List<int> medicosOcupados = new List<int>(); //lista de medicos asignados
+
         private static void Main(string[] args)
         {
-            for (int i = 1; i <= 10; i++)
+            for (int i = 1; i <= 4; i++)
             {
-                Thread paciente = new Thread(()=>Paciente(i));
-                paciente.Start();
-                
-                Thread.Sleep(rnd.Next(1, 10000));
-                
+                Thread paciente = new Thread(Paciente);
+                paciente.Start(i);
+                Thread.Sleep(2000);
             }
-            
-            
-
-            
-
-
-            
         }
 
-        static void Paciente(int id)
+        static void Paciente(object id)
         {
-            Console.WriteLine("Paciente {0} generado",id);
+            int pacienteId = (int)id;
+            int medico;
+
+            // Asignar médico disponible
+            lock (locker)
+            {
+                do
+                {
+                    medico = rnd.Next(1, 5);
+                } while (medicosOcupados.Contains(medico));
+                medicosOcupados.Add(medico);
+            }
+
+            Console.WriteLine("Paciente {0} en sala de espera", pacienteId);
+            sem.Wait();
+
+            Console.WriteLine("  Paciente {0} en consulta con medico Nº {1}", pacienteId, medico);
+            Thread.Sleep(10000);
+            Console.WriteLine("    Paciente {0} finaliza con medico Nº {1}", pacienteId, medico);
+
+            // Liberar médico
+            lock (locker)
+            {
+                medicosOcupados.Remove(medico);
+            }
+
+            sem.Release();
         }
     }
 }
